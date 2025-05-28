86fd1bd2e199470e576c7946b2713e6b7d8cddff
Emmakaranja1 <karanjaemmak@gmail.com>
2025-05-29 00:53:38 +0300

    Adds dotenv package

--- db/config.py
+++ db/config.py
@@ -1,7 +1,9 @@
 """
 contains database configuration
 """
-
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql:Emma@2025@localhost:5432/health_simplified_db")
